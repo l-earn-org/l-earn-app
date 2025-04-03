2a55cb8a878bfcbb83a1798d07b8a9f70f4e11b1
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-04-03 18:06:03 +0300

    feat: add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import heroImage from '../assets/landing-learn.png'
+export const Hero = () => {
+  return (
+    <div className="hero-section">
+      <div className="left">
+        <h1>Creating the necessity for innovation through learning</h1>
+        <p>
+          Join us today and get skills and right networks in your area of
+          interest.
+        </p>
+        <button>Join us for Free</button>
+      </div>
+      <div className="right">
+        <img src={heroImage} />
+      </div>
+    </div>
+  );
+};
