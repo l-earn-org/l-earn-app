966be49f492ea0766ad1a1a69dda8fbd74883b18
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-04-04 17:02:31 +0300

    feat: add navigation in the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
-export const Home = ()=> {
-    return(
-        <div>
-            Home
-        </div>
-    )
+import "../styles/Home.css";
+import {HomeNav} from '../components/HomeNav.jsx'
+// import { projects } from '../data/projects';
+
+export const Home = () => {
+  return (
+    <>
+      <HomeNav />
+      {/* <UserProjects />
+      <Hackathons /> */}
+    </>
+  );
+};
+
+{
+  /* <h1>Hello</h1>
+       {projects.map((item,i) => {
+         return(
+            <div key={i}>
+                <img src={item.image}/>
+                {item.title}
+                {item.theme}
+                {item.date}
+            </div>
+            
+         )
+       })}  */
 }
