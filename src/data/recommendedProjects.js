a90bbc6d9544feb385dc03659d1eeb333169d1ea
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-04-05 16:30:45 +0300

    feat: add recommended projects section and data

---
+++ src/data/recommendedProjects.js
@@ -0,0 +1,28 @@
+import image5 from '../assets/image 15.png'
+import image6 from '../assets/image 16.png'
+import image7 from '../assets/image 17.png'
+import image8 from '../assets/image 18.png'
+
+export const recommendedProjects = [
+    {
+        image: image5,
+        title: 'Solana Ignition Hackathon',
+        theme: 'Compete in building high-performance blockchain applications on Solana’s fast ecosystem.',
+        date: '24th - 30th March, 2025'
+    }, {
+        image: image6,
+        title: 'AI for Good Hackathon',
+        theme: 'Leverage AI and ML to create solutions that address global issues such as healthcare, education, and sustainability.',
+        date: '24th - 30th March, 2025'
+    }, {
+        image: image7,
+        title: 'OpenAInnovation Challenge',
+        theme: 'Develop groundbreaking applications using OpenAI’s powerful language models and APIs.',
+        date: '24th - 30th March, 2025'
+    }, {
+        image: image8,
+        title: 'Google Cloud Buildathon',
+        theme: 'A hackathon for cloud enthusiasts to build scalable solutions using Google Cloud services.',
+        date: '24th - 30th March, 2025'
+    },
+]
